e8ab404d7f2b39fc59800ead65c743bb83704554
byun618 <byun618@gmail.com>
2022-02-06 16:22:10 +0900

    도커 빌드 구현

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import sys
+from vbs import run as vbs_run
+
+if __name__ == '__main__':
+    ticker = sys.argv[1]
+    if not ticker or ticker == None:
+        print('티커를 입력하세요.')
+        sys.exit(1)
+    
+    vbs_run(ticker)
+
+   

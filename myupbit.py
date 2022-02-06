c6f1b19b75af23cfb293fbd0ccbf9c7d6bd7bd91
byun618 <byun618@gmail.com>
2022-02-06 14:59:08 +0900

    vbs 자동 매매 구현

---
+++ myupbit.py
@@ -0,0 +1,126 @@
+import time
+import pandas as pd
+pd.options.display.float_format = '{:.5f}'.format
+import pyupbit
+from datetime import datetime, timedelta
+from utils import dict_to_df, print_df, print_date
+import warnings
+warnings.filterwarnings(action='ignore')
+
+
+class MyUpbit:
+    def __init__(self, access, secret):
+        self.upbit = pyupbit.Upbit(access=access, secret=secret)
+        self.t_start = datetime.now()
+        self.t_prev = self.t_start - timedelta(days=1)
+        self.t_next = self.t_start + timedelta(days=1)
+        self.ticker = None
+        self.target_price = None
+        self.hold = False
+        self.sold = False
+
+    def print_balances(self):
+        columns = ['자산', '보유수량', '평균매수가']
+        balances = self.upbit.get_balances()
+        df = pd.DataFrame(columns=columns)
+
+        for balance in balances:
+            df = df.append({'자산': balance['currency'], '보유수량': balance['balance'],
+                            '평균매수가': balance['avg_buy_price']}, ignore_index=True)
+
+        df.set_index('자산', inplace=True)
+        print_df(df, '보유 자산')
+
+    def set_ticker(self, ticker):
+        self.ticker = ticker
+
+    def get_ohlcv_base(self, base, days=200):
+        df = pyupbit.get_ohlcv(
+            ticker=self.ticker, interval="minute60", count=days*24)
+        return df.resample('24H', base=base).agg({
+            'open': 'first', 'high': 'max', 'low': 'min', 'close': 'last', 'volume': 'sum', 'value': 'sum'
+        })
+
+    def set_target_price_vbs(self):
+        print_date('[VBS] ', self.ticker, '의 목표가를 설정합니다.')
+        df = self.get_ohlcv_base(base=10)
+        df_today = df.loc[self.t_start.strftime('%Y-%m-%d 10:00:00')]
+        df_yes = df.loc[self.t_prev.strftime('%Y-%m-%d 10:00:00')]
+
+        range = df_yes['high'] - df_yes['low']
+        self.target_price =  df_today['open'] + range * 0.5
+        
+
+        print_date('[VBS] ', self.ticker, ' 목표가: ', self.target_price)
+
+    def get_current_price(self, limit_info=False, verbose=False):
+        return pyupbit.get_current_price(ticker=self.ticker, limit_info=limit_info, verbose=verbose)
+
+    def buy_market_order(self):
+        cash = self.upbit.get_balance()
+        
+        try:
+            res = self.upbit.buy_market_order(self.ticker, cash * 0.9995)
+            res_df = dict_to_df(res)
+            print_df(res_df, self.ticker, ' 매수 주문 완료')
+            
+            time.sleep(1)
+        
+            while True:
+                order = self.upbit.get_order(res['uuid'])
+                
+                if order != None and len(order['trades']) > 0:
+                    order_df = dict_to_df(order)
+                    volume = self.upbit.get_balance(self.ticker)
+
+                    if volume == None:
+                        continue
+
+                    print_df(order_df, self.ticker, '(', volume, ')', ' 매수 주문 처리 완료')
+                    return True
+                else:
+                    print_df(self.ticker, ' 매수 주문 처리 대기 중...')                
+                    time.sleep(0.5)
+        except: 
+            print_date(self.ticker, ' 매수 주문 실패')
+            return False
+            
+    def sell_market_order(self):
+        volume = self.upbit.get_balance(self.ticker)
+        
+        if volume == None:
+            return False
+        
+        try:
+            res = self.upbit.sell_market_order(self.ticker, volume)
+            
+            res_df = dict_to_df(res)
+            print_df(res_df, self.ticker, ' 매도 주문 완료')
+            
+            time.sleep(1)
+            
+            while True:
+                order = self.upbit.get_order(res['uuid'])
+                if order != None and len(order['trades']) > 0:
+                    order_df = dict_to_df(order)
+                    
+                    cash = self.upbit.get_balance()
+
+                    if cash == None:
+                        continue
+                    
+                    print_df(order_df, self.ticker, ' 매도 주문 처리 완료')
+                    return True
+                else:
+                    print_df(self.ticker, ' 매수 주문 처리 대기 중...')
+                    time.sleep(0.5)      
+        except:
+            print_date(self.ticker, ' 매도 주문 실패')
+            return False
+        
+            
+        
+        
+            
+        
+        
---
+++ vbs.py
@@ -0,0 +1,72 @@
+import time
+from datetime import datetime
+from myupbit import MyUpbit
+from utils import print_date, dict_to_df, print_df
+
+myupbit = MyUpbit(access='access',
+                  secret='secreet')
+
+
+def run(ticker):
+    print_date('프로그램을 시작합니다.')
+
+    myupbit.print_balances()
+    myupbit.set_ticker(ticker)
+
+    hold = False
+    sold = False
+    i = 0
+    while True:
+        t_now = datetime.now()
+        t_buy = myupbit.t_start.replace(
+            hour=10, minute=5, second=0, microsecond=0)
+        t_sell = myupbit.t_next.replace(
+            hour=9, minute=45, second=0, microsecond=0)
+        t_exit = myupbit.t_next.replace(
+            hour=9, minute=50, second=0, microsecond=0)
+
+        # AM 10:05 ~ AM 09:45 : 매수
+        # 몇시 이전에 매수 하지 못하면 탄력이 없는것으로 판단하는건 어떤가
+        if not hold and t_buy < t_now < t_sell:
+            if not myupbit.target_price:
+                myupbit.set_target_price_vbs()
+
+            current_price = myupbit.get_current_price()
+
+            if current_price > myupbit.target_price:
+                res = myupbit.buy_market_order()
+
+                if res:
+                    hold = True
+
+        # AM 09:45 ~ AM 09:50 : 매도
+        if hold and not sold and t_sell < t_now < t_exit:
+            res = myupbit.sell_market_order()
+
+            if res:
+                hold = False
+                sold = True
+
+        if t_exit < t_now:
+            break
+
+        if i == 10:
+            current_price = myupbit.get_current_price()
+            if not hold and not sold:
+                status = '매수 대기 중'
+            if hold:
+                status = '매도 대기 중'
+            if not hold and sold:
+                status = '매도 완료'
+
+            print_df(dict_to_df({'코인':  myupbit.ticker, '현재가': current_price,
+                     '목표가': myupbit.target_price, '상태': status}), '현황')
+            myupbit.print_balances()
+
+            i = 0
+        i += 1
+
+        time.sleep(1)
+
+    print_date('프로그램을 종료합니다.')
+    myupbit.print_balances()

2885d43ceeca0bbbed05fe97c0fe4d48b9263f0b
byun618 <byun618@gmail.com>
2022-02-06 21:08:07 +0900

    argv로 컨트롤

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-
+import sys
 from public.env import load_env
 from algorithms.vbs import Vbs
 
@@ -6,6 +6,13 @@ from algorithms.vbs import Vbs
 if __name__ == '__main__':
     UPBIT_ACCESS, UPBIT_SECRET = load_env()
 
-    vbs_btc = Vbs(access=UPBIT_ACCESS, secret=UPBIT_SECRET, ticker='KRW-CHZ')
+    argvs = sys.argv
+    algorithm = argvs[1]
+    ticker = argvs[2]
 
-    vbs_btc.start()
+    # 여러 알고리즘을 하나의 키로 동시에 돌리게 되면
+    # 요청 제한에 걸릴 수도, 큐를 사용해야겠네
+
+    if algorithm == 'vbs':
+        vbs = Vbs(access=UPBIT_ACCESS, secret=UPBIT_SECRET, ticker=ticker)
+        vbs.start()
